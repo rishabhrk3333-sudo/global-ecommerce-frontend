8196a617f7325484d55b91521b918f0e5d99f902
rishabhrk3333-sudo <rishabhrk3333@gmail.com>
2026-08-28 16:06:50 +0530

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'Products',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('../app/features/auth/login/login').then(m => m.Login)
+
+    },
+    {
+        path: 'products',
+        loadComponent: () => import('../app/features/products/product-list/product-list').then(m => m.ProductList)
+    },
+    {
+        path: 'products/:id',
+        loadComponent: () => import('../app/features/products/product-details/product-details').then(m => m.ProductDetails)
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('../app/features/cart/cart/cart').then(m => m.Cart)
+    },
+    {
+        path: 'checkout',
+        loadComponent: () => import('./features/checkout/checkout/checkout')
+                .then(m => m.Checkout)
+    },
+
+    {
+        path: 'orders',
+        loadComponent: () =>
+            import('./features/orders/order-list/order-list')
+                .then(m => m.OrderList)
+    },
+
+    {
+        path: 'orders/:id',
+        loadComponent: () =>
+            import('./features/orders/order-details/order-details')
+                .then(m => m.OrderDetails)
+    },
+
+    {
+        path: 'admin/dashboard',
+        loadComponent: () =>
+            import('./features/admin/dashboard/dashboard')
+                .then(m => m.Dashboard)
+    },
+
+    {
+        path: '**',
+        redirectTo: 'products'
+    }
+];
--- src/app/shared/services /product.service/product.ts
+++ src/app/shared/services /product.service/product.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product as ProductModel } from '../../models/product.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Product {}
+export class Product {
+  private readonly http = inject(HttpClient);
+
+  private readonly productsUrl = 'assets/data/products.json';
+
+  getProducts(): Observable<ProductModel[]> {
+    return this.http.get<ProductModel[]>(this.productsUrl);
+  }
+
+  getProductById(id: number): Observable<ProductModel | undefined> {
+    return this.getProducts().pipe(
+      map(products => products.find(item => item?.id === id))
+    );
+  }
+}
